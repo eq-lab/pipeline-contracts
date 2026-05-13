77949c37cee2cd030fb0ae120ea5cb0317add710
Pavel <pavsmel@hotmail.com>
2026-05-13 14:19:08 +0300

    rename hoodie -> hoodi

--- script/base/ChainValues.sol
+++ script/base/ChainValues.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.34;
 contract ChainValues {
     uint256 constant MAINNET_CHAIN_ID = 1;
     uint256 constant SEPOLIA_CHAIN_ID = 11155111;
-    uint256 constant HOODIE_CHAIN_ID = 560048;
+    uint256 constant HOODI_CHAIN_ID = 560048;
     uint256 constant ANVIL_CHAIN_ID = 31337;
 
     error UnknownChainId(uint256 chainId);
@@ -15,7 +15,7 @@ contract ChainValues {
 
     constructor() {
         _loadChainNames();
-        _loadHoodieValues();
+        _loadHoodiValues();
         _loadAnvilValues();
     }
 
@@ -32,55 +32,55 @@ contract ChainValues {
     function _loadChainNames() private {
         chainNames[MAINNET_CHAIN_ID] = "mainnet";
         chainNames[SEPOLIA_CHAIN_ID] = "sepolia";
-        chainNames[HOODIE_CHAIN_ID] = "hoodie";
+        chainNames[HOODI_CHAIN_ID] = "hoodi";
         chainNames[ANVIL_CHAIN_ID] = "anvil";
     }
 
-    function _loadHoodieValues() private {
-        values[HOODIE_CHAIN_ID]["AccessManagerOwner"] =
+    function _loadHoodiValues() private {
+        values[HOODI_CHAIN_ID]["AccessManagerOwner"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["YieldMintAuthority"] =
+        values[HOODI_CHAIN_ID]["YieldMintAuthority"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["Custodian"] = bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
+        values[HOODI_CHAIN_ID]["Custodian"] = bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
 
-        values[HOODIE_CHAIN_ID]["USDC"] = bytes32(uint256(uint160(0xe198F1EEF83Dd613B874FC3c2D5BAf6C8a4A4597)));
+        values[HOODI_CHAIN_ID]["USDC"] = bytes32(uint256(uint160(0xe198F1EEF83Dd613B874FC3c2D5BAf6C8a4A4597)));
 
         // =========== DepositManagerConfig ===========
-        values[HOODIE_CHAIN_ID]["DepositManager__MinDeposit"] = bytes32(uint256(1_000_000_000));
-        values[HOODIE_CHAIN_ID]["DepositManager__RateLimit__TxLimit"] = bytes32(uint256(5_000_000_000_000));
-        values[HOODIE_CHAIN_ID]["DepositManager__RateLimit__WindowLimit"] = bytes32(uint256(10_000_000_000_000));
-        values[HOODIE_CHAIN_ID]["DepositManager__RateLimit__Window"] = bytes32(uint256(86400));
-        values[HOODIE_CHAIN_ID]["DepositManager__RateLimit__Shift"] = bytes32(uint256(0));
-        values[HOODIE_CHAIN_ID]["DepositManager__Verifier"] =
+        values[HOODI_CHAIN_ID]["DepositManager__MinDeposit"] = bytes32(uint256(1_000_000_000));
+        values[HOODI_CHAIN_ID]["DepositManager__RateLimit__TxLimit"] = bytes32(uint256(5_000_000_000_000));
+        values[HOODI_CHAIN_ID]["DepositManager__RateLimit__WindowLimit"] = bytes32(uint256(10_000_000_000_000));
+        values[HOODI_CHAIN_ID]["DepositManager__RateLimit__Window"] = bytes32(uint256(86400));
+        values[HOODI_CHAIN_ID]["DepositManager__RateLimit__Shift"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["DepositManager__Verifier"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
 
-        values[HOODIE_CHAIN_ID]["LoanRegistry__erc721Name"] = bytes32(bytes("LoanRegistryName"));
-        values[HOODIE_CHAIN_ID]["LoanRegistry__erc721Symbol"] = bytes32(bytes("LRS"));
+        values[HOODI_CHAIN_ID]["LoanRegistry__erc721Name"] = bytes32(bytes("LoanRegistryName"));
+        values[HOODI_CHAIN_ID]["LoanRegistry__erc721Symbol"] = bytes32(bytes("LRS"));
 
-        values[HOODIE_CHAIN_ID]["WithdrawalQueue__TokenHolderMCP"] =
+        values[HOODI_CHAIN_ID]["WithdrawalQueue__TokenHolderMCP"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["WithdrawalQueue__Verifier"] =
+        values[HOODI_CHAIN_ID]["WithdrawalQueue__Verifier"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
 
         // =========== Roles ===========
-        values[HOODIE_CHAIN_ID]["DepositManagerAdmin"] =
+        values[HOODI_CHAIN_ID]["DepositManagerAdmin"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["DepositManagerAdmin__Delay"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["DepositManagerAdmin__Delay"] = bytes32(uint256(0));
 
-        values[HOODIE_CHAIN_ID]["EmergencyRole"] = bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["EmergencyRole__Delay"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["EmergencyRole"] = bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
+        values[HOODI_CHAIN_ID]["EmergencyRole__Delay"] = bytes32(uint256(0));
 
-        values[HOODIE_CHAIN_ID]["LoanRegistryManager"] =
+        values[HOODI_CHAIN_ID]["LoanRegistryManager"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["LoanRegistryManager__Delay"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["LoanRegistryManager__Delay"] = bytes32(uint256(0));
 
-        values[HOODIE_CHAIN_ID]["WhitelistManager"] =
+        values[HOODI_CHAIN_ID]["WhitelistManager"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["WhitelistManager__Delay"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["WhitelistManager__Delay"] = bytes32(uint256(0));
 
-        values[HOODIE_CHAIN_ID]["YieldMinterManager"] =
+        values[HOODI_CHAIN_ID]["YieldMinterManager"] =
             bytes32(uint256(uint160(0xFE1748f511583f6c9349f672593E6312BeDfcE40)));
-        values[HOODIE_CHAIN_ID]["YieldMinterManager__Delay"] = bytes32(uint256(0));
+        values[HOODI_CHAIN_ID]["YieldMinterManager__Delay"] = bytes32(uint256(0));
     }
 
     function _loadAnvilValues() private {
